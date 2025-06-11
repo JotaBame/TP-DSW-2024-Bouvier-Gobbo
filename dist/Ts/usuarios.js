e09e4daa9f0ca2030454247dfcb566f9f34b664c
Gibi <creismen0@gmail.com>
2025-06-11 12:07:27 -0300

    funciona, es lo que importa
    leves cambios

---
+++ dist/Ts/usuarios.js
@@ -0,0 +1,10 @@
+export class Usuario {
+    constructor(name, type, mail, peso, altura) {
+        this.name = name;
+        this.type = type;
+        this.mail = mail;
+        this.peso = peso;
+        this.altura = altura;
+    }
+}
+//# sourceMappingURL=usuarios.js.map
--- dist/app.js
+++ dist/app.js
@@ -1,15 +1,9 @@
 import express from 'express';
-import { Alimento } from './alimento.js';
+import { Usuario } from "./Ts/usuarios.js";
+import { Alimento } from "./Ts/alimento.js";
 const app = express();
+//middleWare para trabajar los datos que nos ingresan con el titulo "application/json" 
 app.use(express.json());
-// get  -> obtener info sobre recursos
-// post -> crear nuevos recursos
-// delete -> borrar recursos
-// put & patch -> modificar recursos
-//alimentos -> /api/characters/
-const alimentos = [new Alimento("Arandanos", "Arcor", null, "gramos"),
-    new Alimento("Pollo", "Pollo27", null, "kilogramos")
-];
 function sanitizeAlimentoInput(req, res, next) {
     req.body.sanitizedInput = {
         // TO DO: No deberia dejar agregar un alimento igual a otro. Hay que validar que tanto nombre y marca sean distintos.
@@ -26,6 +20,86 @@ function sanitizeAlimentoInput(req, res, next) {
     });
     next();
 }
+function sanitizeUsuarioInput(req, res, next) {
+    req.body.sanitizedInput = {
+        // TO DO: No deberia dejar agregar un alimento igual a otro. Hay que validar que tanto nombre y marca sean distintos.
+        // TO DO: Evitar que puedan ingresarse tipos de datos no validos.
+        name: req.body.name,
+        type: req.body.type,
+        mail: req.body.mail,
+        peso: req.body.peso,
+        altura: req.body.altura
+    };
+    Object.keys(req.body.sanitizedInput).forEach((key) => {
+        if (req.body.sanitizedInput[key] === undefined) {
+            delete req.body.sanitizedInput[key];
+        }
+    });
+    next();
+}
+const usuarios = [
+    new Usuario('pepe', 'free', 'pepepiola@gmail.com', 76, 175),
+    new Usuario('juan', 'vip', 'juanpiola@gmail.com', 83, 172),
+];
+app.get('/api/usuarios', (req, res) => {
+    res.json(usuarios);
+});
+app.get('/api/usuarios/:name', (req, res) => {
+    const usuario = usuarios.find((usuario) => usuario.name === req.params.name);
+    if (!usuario) {
+        res.status(404).send({ message: 'usuario no encontrado' });
+    }
+    res.json(usuario);
+});
+app.post('/api/usuarios', sanitizeUsuarioInput, (req, res) => {
+    //body sería la info del nuevo usuario que nos ingresan
+    const { name, type, mail, peso, altura } = req.body;
+    const new_usuario = new Usuario(name, type, mail, peso, altura);
+    usuarios.push(new_usuario);
+    // el status 201 indica que se creó un nuevo recurso
+    res.status(201).send({ message: 'usuario creado', data: new_usuario });
+});
+app.put('/api/usuarios/:name', sanitizeUsuarioInput, (req, res) => {
+    const usuarioname = usuarios.findIndex((usuario) => usuario.name === req.params.name);
+    if (usuarioname === -1) //no lo encontró
+     {
+        res.status(404).send({ message: 'Usuario no encontrado' });
+    }
+    const input = {
+        name: req.body.name,
+        type: req.body.type,
+        mail: req.body.mail,
+        peso: req.body.peso,
+        altura: req.body.altura
+    };
+    usuarios[usuarioname] = { ...usuarios[usuarioname], ...input };
+    res.status(200).send({ message: 'Usuario actualizado correctamente', data: alimentos[usuarioname] });
+});
+app.patch('/api/usuarios/:name', sanitizeUsuarioInput, (req, res) => {
+    const usuario_peso = usuarios.findIndex((usuario) => usuario.name === req.params.name);
+    if (usuario_peso === -1) {
+        res.status(404).send({ message: 'Usuario no encontrado' });
+        return;
+    }
+    const input = {
+        peso: req.body.peso
+    };
+    usuarios[usuario_peso] = { ...usuarios[usuario_peso], ...input };
+    res.status(200).send({ message: 'Atributo actualizado correctamente', data: alimentos[usuario_peso] });
+});
+app.delete('/api/usuarios/:name', (req, res) => {
+    const usuario_ = usuarios.findIndex((usuario) => usuario.name === req.params.name);
+    if (usuario_ === -1) {
+        res.status(404).send({ message: 'Usuario no encontrado' });
+        return;
+    }
+    usuarios.splice(usuario_, 1);
+    res.status(200).send({ message: 'usuarioo borrado exitosamente' });
+});
+//Comienza el CRUD alimentos
+const alimentos = [new Alimento("Arandanos", "Arcor", null, "gramos"),
+    new Alimento("Pollo", "Pollo27", null, "kilogramos")
+];
 app.get('/api/alimentos', (req, res) => {
     res.json(alimentos);
 });
@@ -70,5 +144,7 @@ app.delete('/api/alimentos/:nombre', (req, res) => {
     alimentos.splice(alimentoNombre, 1);
     res.status(200).send({ message: 'Alimento borrado exitosamente' });
 });
-app.listen(3000, () => { console.log("server running on http://localhost:3000/"); });
+app.listen(3000, () => {
+    console.log("funka");
+});
 //# sourceMappingURL=app.js.map
