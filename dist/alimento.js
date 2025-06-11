28bfad5cc17d072e8410fdf00865dd0fe7c6b69c
= <juanbouvier5@gmail.com>
2025-06-11 16:13:47 -0300

    a ver si se soluciono

--- dist/alimento.js
+++
@@ -1,9 +0,0 @@
-export class Alimento {
-    constructor(nombre, marca, presentacion, unidadMedida) {
-        this.nombre = nombre;
-        this.marca = marca;
-        this.presentacion = presentacion;
-        this.unidadMedida = unidadMedida;
-    }
-}
-//# sourceMappingURL=alimento.js.map
--- dist/index.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-console.log("sexo");
-//# sourceMappingURL=index.js.map
